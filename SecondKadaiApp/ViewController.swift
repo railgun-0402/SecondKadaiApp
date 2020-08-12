c81ab54c116488c8570968241725125625ab7901
sugawaradaiki <april-life.f-441-2011@docomo.ne.jp>
2020-08-12 14:24:04 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 菅原大輝 on 2020/08/12.
+//  Copyright © 2020 菅原大輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
